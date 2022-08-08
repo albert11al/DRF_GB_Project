a7dc8ac283e25e4c8848ab96883aa2250b63b8c7
Albert <albert11al@example.com>
2022-08-08 11:15:10 +0300

    Migrate

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class MyUser(AbstractUser):
+    first_name = models.CharField(max_length=64, blank=True)
+    last_name = models.CharField(max_length=64, blank=True)
+    birthday_year = models.PositiveIntegerField(blank=True)
+    email = models.EmailField(unique=True)
