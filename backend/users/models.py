47182fb3f4b8c95d7faf80ba3eccbee3abad432d
Albert <albert11al@example.com>
2022-08-09 14:17:17 +0300

    List of users from several components

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import AbstractBaseUser
 
 # Create your models here.
 
 class MyUser(AbstractUser):
+    username = None
     first_name = models.CharField(max_length=64, blank=True)
     last_name = models.CharField(max_length=64, blank=True)
     birthday_year = models.PositiveIntegerField(null=True, blank=True)
     email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,50 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserList from './components/UserList.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            'users': []
+        }
+    }
+    componentDidMount() {
+        const users = [
+            {
+                "first_name": "dlon",
+                "last_name": "brovic",
+                "birthday_year": 1896,
+                "email": "a@a.com"
+            },
+            {
+                "first_name": "alon",
+                "last_name": "kombo",
+                "birthday_year": 1985,
+                "email": "ad@em.com"
+            },
+            {
+                "first_name": "drinc",
+                "last_name": "pilovic",
+                "birthday_year": 2000,
+                "email": "tren@ren.com"
+            }
+        ]
+        this.setState(
+            {
+                'users': users
+            }
+        )
+    }
+
+    render () {
+        return (
+            <div>
+                <UserList users={this.state.users} />
+            </div>
+        )
+    }
 }
 
 export default App;
