36928fba8f1f29902e7fd36bd44607063ec0b241
Albert <albert11al@example.com>
2022-08-19 12:11:14 +0300

    API: model Project

--- backend/TODO/views.py
+++ backend/TODO/views.py
@@ -1,14 +1,31 @@
+from rest_framework import mixins
+from rest_framework.pagination import LimitOffsetPagination
 from TODO.models import Project, Todo
 from TODO.serializers import ProjectSerializer, TodoSerializer
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
-class ProjectViews(ModelViewSet):
+# для постраничного вывода установить размер страницы 10 записей;
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+#доступны все варианты запросов;
+class ProjectViews(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    GenericViewSet,
+):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    pagination_class = ProjectLimitOffsetPagination
+    def get_queryset(self):
+        return Project.objects.filter(name_todo='проект')
 
 class TodoViews(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+
 
 
 
