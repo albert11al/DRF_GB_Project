9dba75277c8646538530532c33fdc1a6ff4db913
albert11al <67281370+albert11al@users.noreply.github.com>
2022-08-21 08:05:59 +0300

    Lesson 4 (#4)
    Lesson_4 lesson 4 ready

--- backend/TODO/views.py
+++ backend/TODO/views.py
@@ -1,14 +1,48 @@
+from rest_framework import mixins
+from rest_framework.pagination import LimitOffsetPagination
 from TODO.models import Project, Todo
 from TODO.serializers import ProjectSerializer, TodoSerializer
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet
 
-class ProjectViews(ModelViewSet):
+# для постраничного вывода установить размер страницы 10 записей;
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+#доступны все варианты запросов;
+class ProjectViews(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    GenericViewSet,
+):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-
-class TodoViews(ModelViewSet):
+    pagination_class = ProjectLimitOffsetPagination
+    def get_queryset(self):
+        return Project.objects.filter(name_todo__contains='проект')
+class TodoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
+class TodoViews(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    GenericViewSet,
+):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    pagination_class = TodoLimitOffsetPagination
+    #добавить фильтрацию по проекту
+    filterset_fields = ['project']
+# при удалении не удалять Todo а выставлять признак, что оно закрыто
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
+
+
+
 
 
 
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'corsheaders',
     'users',
@@ -136,3 +137,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    #'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    #'PAGE_SIZE': 100
+}
